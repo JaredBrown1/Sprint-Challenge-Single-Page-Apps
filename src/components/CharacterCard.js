9ba4fdf0887f4de43927747a63629a447195593a
Jared Brown <jared@Jareds-MBP.lan1>
2019-12-14 09:45:18 -0800

    added character list and character card from api

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,11 @@
 import React from "react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard(name, species, status) {
+  return (
+    <div>
+      <h3>Name:{name}</h3>
+      <p>Species:{species}</p>
+      <p>Status:{status}</p>
+    </div>
+  );
 }
