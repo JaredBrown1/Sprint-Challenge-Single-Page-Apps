739796d019a8ca424fc4ef475e0f7072ee9a2282
Jared Brown <jared@Jareds-MBP.lan1>
2019-12-14 10:00:35 -0800

    search form added

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,51 @@
 import React, { useState } from "react";
+import axios from "axios";
+import CharacterCard from "./CharacterCard";
+import { Link } from "react-router-dom";
 
 export default function SearchForm() {
- 
+  const [data, setData] = useState([]);
+  const [query, setQuery] = useState("");
+
+  useEffect(() => {
+    axios.get("https://rickandmortyapi.com/api/character/").then(response => {
+      const characters = response.data.results.filter(char =>
+        char.name.toLowerCase().includes(query.toLowerCase())
+      );
+      setData(characters);
+    });
+  }, [query]);
+
+  const handleInputChange = e => {
+    setQuery(event.target.value);
+  };
+
   return (
-    <section className="search-form">
-     // Add a search form here
-    </section>
+    <div>
+      <form>
+        <input
+          id="name"
+          type="text"
+          name="textfield"
+          placeholder="Search"
+          value={query}
+          onChange={handleInputChange}
+        />
+        <Link to="/">
+          <button>Home</button>
+        </Link>
+      </form>
+
+      {data.map(char => {
+        return (
+          <CharacterCard
+            key={char.id}
+            name={char.name}
+            species={char.species}
+            status={char.status}
+          />
+        );
+      })}
+    </div>
   );
 }
